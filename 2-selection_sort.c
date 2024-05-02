4e431068b3f556ab56dbd54e75b4b76db443c18b
Victoria Afuwape <victoriaafuwape@outlook.com>
2024-05-02 17:17:45 +0100

    Task 2

---
+++ 2-selection_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in ascending order using the
+ *                  Selection sort algorithm
+ * @array: an array of integers to be sorted
+ * @size: the size of the array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
